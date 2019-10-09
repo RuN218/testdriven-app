cd8907a5b8fbf23d468b6651a74c3400bf72fa55
Ruslan <218.run@gmail.com>
2019-10-09 17:04:39 +0300

    Jinja Templates

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -131,6 +131,39 @@ class TestUserService(BaseTestCase):
                 "fletcher@notreal.com", data["data"]["users"][1]["email"])
             self.assertIn("success", data["status"])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been
+        added to the database."""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"<h1>All Users</h1>", response.data)
+        self.assertIn(b"<p>No users!</p>", response.data)
+
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been
+        added to the database."""
+        add_user("ruslan", "218.run@gmail.com")
+        add_user("fletcher", "fletcher@notreal.com")
+        with self.client:
+            response = self.client.get("/")
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b"<h1>All Users</h1>", response.data)
+            self.assertNotIn(b"<h1>No users!</h1>", response.data)
+            self.assertIn(b"ruslan", response.data)
+            self.assertIn(b"fletcher", response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                "/",
+                data=dict(username="ruslan", email="218.run@gmail.com"),
+                follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b"<h1>All Users</h1>", response.data)
+            self.assertNotIn(b"<h1>No users!</h1>", response.data)
+            self.assertIn(b"ruslan", response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
